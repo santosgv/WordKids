236ec3b7ea6340449b5affc373a2d7878b916cf8
Vitor dos Santos Gomes <54445515+santosgv@users.noreply.github.com>
2023-12-27 17:50:52 -0300

    conversao img preto e branco donwload

--- Backend/Core/views.py
+++ Backend/Core/views.py
@@ -12,7 +12,7 @@ from rest_framework.decorators import action
 from django.http import JsonResponse,HttpResponse
 from django.views.decorators.cache import cache_page
 from django.utils.decorators import method_decorator
-
+from PIL import Image
 
 
 
@@ -50,10 +50,37 @@ def imprimir(request,id):
         try:
             image = Imagem.objects.get(id=id)
             buffer = io.BytesIO()
-            PDF = canvas.Canvas(buffer,pagesize=letter)
-            PDF.drawImage(os.path.join(settings.BASE_DIR, 'media',f'{image.arquivo}'),0, 0, width=letter[0], height=letter[1])
+            PDF = canvas.Canvas(buffer, pagesize=letter)
+
+            # Abre a imagem usando PIL
+            img_path = os.path.join(settings.BASE_DIR, 'media', f'{image.arquivo}')
+            img = Image.open(img_path)
+
+            # Converte a imagem para preto e branco
+            img = img.convert("L")
+
+            # Obtém as dimensões da folha A4
+            a4_width, a4_height = letter
+
+            # Calcula as proporções para manter a escala
+            width_ratio = a4_width / img.width
+            height_ratio = a4_height / img.height
+            min_ratio = min(width_ratio, height_ratio)
+
+            # Calcula as novas dimensões da imagem mantendo a escala
+            new_width = int(img.width * min_ratio)
+            new_height = int(img.height * min_ratio)
+
+            # Calcula as coordenadas para centralizar a imagem na folha
+            x_offset = (a4_width - new_width) / 2
+            y_offset = (a4_height - new_height) / 2
+
+            # Desenha a imagem na folha A4 mantendo a escala
+            PDF.drawInlineImage(img, x_offset, y_offset, width=new_width, height=new_height)
+
             PDF.showPage()
             PDF.save()
+
             buffer.seek(0)
             response = FileResponse(buffer, as_attachment=True, filename='Mundo Colorido Kids - Desenho.pdf')
             response.status_code = 200
