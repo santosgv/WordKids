9363cc5722e0b43b5283bdc77d471ea5b6ac9a18
Vitor <santosgomesv@gmail.com>
2024-01-03 00:05:09 -0300

    teste

--- Backend/Word/settings.py
+++ Backend/Word/settings.py
@@ -156,15 +156,7 @@ CORS_ALLOW_CREDENTIALS = True
 
 
 CORS_ALLOW_HEADERS = [
-    'accept',
-    'accept-encoding',
-    'authorization',
-    'content-type',
-    'dnt',
-    'origin',
-    'user-agent',
-    'x-csrftoken',
-    'x-requested-with',
+    'Content-Type'
 ]
 
 CORS_ALLOWED_ORIGINS = [
