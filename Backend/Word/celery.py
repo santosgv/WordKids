1789ba93de1a2e7665cb0b3f23563e5e129cdf8a
Vitor dos Santos Gomes <54445515+santosgv@users.noreply.github.com>
2024-01-29 19:03:21 -0300

    utc

--- Backend/Word/celery.py
+++ Backend/Word/celery.py
@@ -4,6 +4,10 @@ from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE','Word.settings')
 
 app =Celery('Word')
+app.conf.enable_utc= False
+app.conf.update(timezone ='America/Sao_Paulo')
+
+app.config_from_object('django.conf.settings',namespace='CELERY')
 
 app.autodiscover_tasks()
 
