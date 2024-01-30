7b90f4e71f9723b1f9af6cd52a96131cbd14897c
Vitor dos Santos Gomes <54445515+santosgv@users.noreply.github.com>
2024-01-29 21:04:50 -0300

    voltando funcao de download

--- Backend/Core/views.py
+++ Backend/Core/views.py
@@ -8,6 +8,11 @@ from django.views.decorators.cache import cache_page
 from django.core.cache import cache
 from django.db.models import Count
 from .tasks import tastk_imprimir
+from PIL import Image
+import io
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter
+from django.http import FileResponse
 
 def get_categorias_com_contagem():
     cached_categorias = cache.get('all_categorias_com_contagem')
@@ -76,8 +81,47 @@ def transparencia(request):
      return render(request,'transparencia.html',{'categorias':categorias,})
 
 def imprimir(request,id):
-    tastk_imprimir.delay(id)
-    return None
+        try:
+            image = Imagem.objects.get(id=id)
+            buffer = io.BytesIO()
+            PDF = canvas.Canvas(buffer, pagesize=letter)
+
+            # Abre a imagem usando PIL
+            img_path = os.path.join(settings.BASE_DIR, 'media', f'{image.arquivo}')
+            img = Image.open(img_path)
+
+            # Converte a imagem para preto e branco
+            img = img.convert("L")
+
+            # Obtém as dimensões da folha A4
+            a4_width, a4_height = letter
+
+            # Calcula as proporções para manter a escala
+            width_ratio = a4_width / img.width
+            height_ratio = a4_height / img.height
+            min_ratio = min(width_ratio, height_ratio)
+
+            # Calcula as novas dimensões da imagem mantendo a escala
+            new_width = int(img.width * min_ratio)
+            new_height = int(img.height * min_ratio)
+
+            # Calcula as coordenadas para centralizar a imagem na folha
+            x_offset = (a4_width - new_width) / 2
+            y_offset = (a4_height - new_height) / 2
+
+            # Desenha a imagem na folha A4 mantendo a escala
+            PDF.drawInlineImage(img, x_offset, y_offset, width=new_width, height=new_height)
+            PDF.drawString(120,2,'Visite https://mundocoloridokids.com.br para mais desenhos')
+
+            PDF.showPage()
+            PDF.save()
+
+            buffer.seek(0)
+            response = FileResponse(buffer, as_attachment=True, filename='Mundo Colorido Kids - Desenho.pdf')
+            response.status_code = 200
+            return response
+        except Exception as msg:
+            response = msg
 
 
 @cache_page(60 * 100)
